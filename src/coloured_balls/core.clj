af301feb4293885431a202803424ba02fbd8c25a
Philip Potter <philipgpotter@gmail.com>
2011-05-04 22:22:54 +0100

    team 2! yeah!

--- src/coloured_balls/core.clj
+++ src/coloured_balls/core.clj
@@ -1,6 +1,7 @@
 (ns coloured-balls.core
   (:use [rosado.processing]
-        [rosado.processing.applet])
+        [rosado.processing.applet]
+	[coloured-balls.motion])
   (:gen-class))
 
 ;; here's a function which will be called by Processing's (PApplet)
@@ -13,12 +14,27 @@
 	(ellipse (:x ball) (:y ball) (:radius ball) (:radius ball)))
 
 (defn make-ball []
-	{:x (rand-int 400) :y (rand-int 400) :red (rand-int 256) :blue (rand-int 256) :green (rand-int 256) :radius (+ 1 (rand-int 70))})
+  {:x (+ 25 (rand-int 350)) :y (+ 25 (rand-int 350)) :red (rand-int 256) :blue (rand-int 256) :green (rand-int 256) :radius (+ 1 (rand-int 70))})
+
+(def ball (conj (make-ball) {:velocity 15 :heading (rand-int 360)}))
 
 (defn draw
   "Example usage of with-translation and with-rotation."
   []
-	(draw-ball (make-ball))
+  (def ball ball)
+  (background-float 125)
+  (draw-ball ball)
+  (when (or 
+	 (> (:y ball) (- 400 (/ (ball :radius) 2)))
+	 (< (:y ball) (/ (ball :radius) 2)))
+    (def ball (conj ball {:heading (- 360 (:heading ball)) :radius (+ 1 (:radius ball))}))
+    )
+  (when (or 
+	 (> (:x ball) (- 400 (/ (ball :radius) 2)))
+	 (< (:x ball) (/ (ball :radius) 2)))
+    (def ball (conj ball {:heading (- 180 (:heading ball)) :radius (+ 1 (:radius ball))}))
+    )
+  (def ball (move ball))
   )
 
 (defn setup []
@@ -34,5 +50,5 @@
   :setup setup :draw draw :size [400 400])
 
 (defn -main [& args]
- (run balls))
+ (run balls true))
 
